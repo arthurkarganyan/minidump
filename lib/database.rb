8587ea00f6a77893b5cbd710b1eb2b2ded9740a1
Arthur Karganyan <arthur.karganyan@gmail.com>
2020-01-08 10:37:17 +0100

    better log

--- lib/database.rb
+++ lib/database.rb
@@ -16,20 +16,24 @@ class Database
 
   def dump!
     new_filename = "#{name}.#{self.class.name}.#{timestamp}.dump.gz"
-    file_path = "#{new_filename}"
-    `#{dump_cmd(file_path)}`
-    `tar czf #{new_filename} dump`
-    `rm -r dump`
+    run_sh dump_cmd(new_filename)
+    run_sh "tar czf #{new_filename} dump"
+    run_sh "rm -r dump"
     path = ENV['RCLONE_CONFIG']
     f = File.open(path).read.split("\n").map(&:strip).select { |i| i[0] != '#' }
     swift_storage_name = f[0][1..-2]
-    cmd = "rclone --config #{path} copy #{file_path} #{swift_storage_name}:#{name}.#{self.class.name}"
-    App.logger.info("Running: #{cmd}")
+    cmd = "rclone --config #{path} copy #{new_filename} #{swift_storage_name}:#{name}.#{self.class.name}"
+    App.logger.info(cmd)
     stdout, stderr, status = Open3.capture3(cmd)
     App.logger.info stdout
     App.logger.info stderr
 
-    `rm #{file_path}`
+    run_sh "rm #{new_filename}"
+  end
+
+  def run_sh(cmd)
+    App.logger.info cmd
+    `#{cmd}`
   end
 
 end
